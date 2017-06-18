29a68838b40d640effab90c6f4e807d777816351
Mina Nielsen <m@m9n.uk>
2017-06-18 14:26:39 +0100

    Initialised with blank plugin file. *rolls up sleeves*

---
+++ tinyevents.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Tiny Events
+Plugin URI: https://github.com/m9n/tinyevents
+Description: A minimal events plugin for WordPress. Does the basics with gusto. Packed with ideas for extending it "the WordPress way", to perfectly suit your needs.
+Version: 0.1
+Author: Mina Nielsen
+Author URI: http://minanielsen.net
+License: GPL V2 or higher
+License URI: https://github.com/m9n/tinyevents/blob/master/LICENSE
+*/
